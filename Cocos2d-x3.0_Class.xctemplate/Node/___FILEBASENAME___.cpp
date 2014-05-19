c19404fe4d62c1c46fed44244c7d2c2f43368bba
gc-yokomichi <yokomichi@gcrest.com>
2014-05-19 09:18:26 +0900

    add Node class template

---
+++ Cocos2d-x3.0_Class.xctemplate/Node/___FILEBASENAME___.cpp
@@ -0,0 +1,36 @@
+/**
+
+@file  ___FILENAME___
+
+@brief description
+
+___PROJECTNAME___
+
+@author ___FULLUSERNAME___
+@date ___DATE___
+___COPYRIGHT___
+*/
+
+#include "___FILEBASENAME___.h"
+
+USING_NS_CC;
+
+___FILEBASENAME___::___FILEBASENAME___()
+{
+    
+}
+
+___FILEBASENAME___::~___FILEBASENAME___()
+{
+    
+}
+
+bool ___FILEBASENAME___::init()
+{
+    if (!___VARIABLE_cocos2DX3.0Subclass:identifier___::init()) {
+        return false;
+    }
+    
+    return true;
+}
+
---
+++ Cocos2d-x3.0_Class.xctemplate/Node/___FILEBASENAME___.h
@@ -0,0 +1,44 @@
+/**
+
+@file ___FILENAME___
+
+@brief description
+
+___PROJECTNAME___
+
+@author ___FULLUSERNAME___
+@date ___DATE___
+___COPYRIGHT___
+*/
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+/**
+@class ___FILEBASENAME___ ___FILENAME___
+
+@brief description
+
+detail description
+
+@author ___FULLUSERNAME___
+@date ___DATE___
+*/
+class ___FILEBASENAME___ : public cocos2d::___VARIABLE_cocos2DX3.0Subclass:identifier___
+{
+    
+public:
+    ___FILEBASENAME___();
+    virtual ~___FILEBASENAME___();
+    virtual bool init();
+    CREATE_FUNC(___FILEBASENAME___);
+    
+protected:
+    
+private:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
